ba1abf6254d036fa1b2c0f88b67eb8fbd03bb14d
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-22 22:25:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,25 @@
 def starts_with_a_vowel?(word)
-
+if word == word.match(/^[aiueo]\w/)
+  TRUE
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+/^(un)\w$(ing)/
 end
 
 def words_five_letters_long(text)
-
+/\w{5}/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text = /^[A-Z]$[.,!?]/
+  TRUE
+end
 end
 
 def valid_phone_number?(phone)
-
+if phone == /[0-9]{3}-[0-9]{3}-[0-9]{4}/
+  TRUE
+end
 end
